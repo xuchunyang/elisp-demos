f454951579d0c44687c378091827c4591b758a36
Xu Chunyang <mail@xuchunyang.me>
2018-11-26 14:39:42 +0800

    Add test for sorting elisp-demos.org alphabetically

--- elisp-demos-tests.el
+++ elisp-demos-tests.el
@@ -4,3 +4,8 @@
 (ert-deftest elisp-demos--search ()
   (should (stringp (elisp-demos--search 'mapcar)))
   (should (null (elisp-demos--search 'tom-and-jerry))))
+
+(ert-deftest elisp-demos--symbols ()
+  "Test if the return value is sorted alphabetically."
+  (should (equal (elisp-demos--symbols)
+                 (sort (elisp-demos--symbols) #'string<))))
