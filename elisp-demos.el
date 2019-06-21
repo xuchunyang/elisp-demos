7fc59782adc18cfd4b1cf3455f5fe113028e865a
Xu Chunyang <mail@xuchunyang.me>
2019-06-21 17:39:20 +0800

    Change version to latest git tag

--- elisp-demos.el
+++ elisp-demos.el
@@ -5,7 +5,7 @@
 ;; Author: Xu Chunyang <mail@xuchunyang.me>
 ;; Homepage: https://github.com/xuchunyang/elisp-demos
 ;; Keywords: lisp, docs
-;; Version: 0
+;; Version: 2019.06.21
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
