8a517e8457c42735538c38555c6ac799f33855ef
Xu Chunyang <xuchunyang56@gmail.com>
2019-07-20 21:01:42 +0800

    Bump 2019.07.20

--- elisp-demos.el
+++ elisp-demos.el
@@ -5,7 +5,7 @@
 ;; Author: Xu Chunyang <mail@xuchunyang.me>
 ;; Homepage: https://github.com/xuchunyang/elisp-demos
 ;; Keywords: lisp, docs
-;; Version: 2019.07.13
+;; Version: 2019.07.20
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
